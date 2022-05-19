fc0ae52a7581dbdad16cbd6a0ddf24ba67e6020a
Caroline Owen <carolinaowen95@gmail.com>
2022-05-19 14:20:41 +0200

    page produit

---
+++ front/js/product.js
@@ -0,0 +1,54 @@
+let params = new URL(document.location).searchParams;
+let id = params.get("id");
+console.log(id);
+
+//déclarer URL Produits + /id 
+let mainURL = "http://localhost:3000/api/products/"
+
+let productURL = mainURL + id;
+console.log(productURL)
+
+fetch(productURL)
+.then(result => result.json())
+.then((data) => {
+    let imag = document.createElement("img")
+    document.querySelector(".item__img").appendChild(imag);
+    imag.src = data.imageUrl;
+    let descri = data.description;
+    console.log(descri)
+    document.getElementById("description").innerText += (`${descri}`)
+    document.getElementById("title").innerText += (`${data.name}`)
+    document.getElementById("price").innerText += (`${data.price}`)
+})
+.then((data) => {for(let colors of data) {
+    document.getElementById("colors").innerHTML += (`<option value="vert">${data.colors}</option>
+    <option value="blanc">${data.colors}</option>`)
+}
+
+}
+);
+
+/*fetch(productURL) 
+.then(result => result.json())
+.then((data) => { for(let k in data) {
+    console.log(k);
+    let test = data[k];
+    console.log(test);
+    for(let b in k){
+        console.log(b);
+        console.log(k[name]);
+    }
+    let descri = document.getElementById("description");
+    let info = k.description;
+    console.log(info)
+descri.innerText = info;}
+    
+
+.then((data) => {for(let [key, value] of Object.entries(data)) {
+    console.log(`${data.description}`);
+}});
+
+}) */
+
+
+
